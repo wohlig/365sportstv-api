9c7d82fd1f3743057fb6d2b3f7ab098082acaaad
samayyy <samaykgada09@gmail.com>
2022-07-12 15:59:38 +0530

    changes

--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -42,7 +42,6 @@ router.get(
     "/getTotalExpiredSubscribedUsersForAdmin",
     authenticateAdmin,
     async (req, res) => {
-        console.log("IINNN")
         try {
             const data =
                 await SubscriptionModel.getTotalExpiredSubscribedUsersForAdmin()
