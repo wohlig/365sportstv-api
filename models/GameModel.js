2b70bee949eba51ecb2bfe63dbc08cc727f90a39
omkar-wohlig <omkarwohlig@gmail.com>
2022-07-21 14:07:17 +0530

    changes

--- models/GameModel.js
+++ models/GameModel.js
@@ -238,6 +238,7 @@ export default {
                     _id: 1,
                     name: 1,
                     description: 1,
+                    username: 1,
                     favorite: {
                         $cond: {
                             if: { $eq: ["$favorite", null] },
@@ -426,6 +427,7 @@ export default {
                         $cond: [{ $eq: ["$status", "enabled"] }, true, false]
                     },
                     meetingStatus: 1,
+                    username: 1,
                     liveStatus: 1
                 }
             }
